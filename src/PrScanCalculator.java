e9a5c86550ab571c76f5b09f9fe8b68b3aeca04a
sinjwu <harbirdouk@gmail.com>
2025-06-21 21:16:04 +0900

    250621~22 review 02

--- src/PrScanCalculator.java
+++ src/PrScanCalculator.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 public class PrScanCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("숫자 X: ");
         double num1 = sc.nextDouble();
-
-        System.out.println("연산자 입력: ");
+        System.out.println("숫자1: " + num1);
         char operator = sc.next().charAt(0);
-
-        System.out.println("숫자 Y: ");
+        System.out.println("연산자: " + operator);
         double num2 = sc.nextDouble();
-
+        System.out.println("숫자2: " + num2);
         double result;
 
         switch (operator) {
@@ -27,7 +23,7 @@ public class PrScanCalculator {
                 break;
             case '/':
                 result = num1 / num2;
-                if(num2 == 0) {
+                if (num2 == 0) {
                     System.out.println("나눌 수 없음");
                     return;
                 }
@@ -36,6 +32,6 @@ public class PrScanCalculator {
                 System.out.println("잘못된 입력");
                 return;
         }
-        System.out.println("계산 결과: " + result);
+        System.out.println("결과: " + result);
     }
 }
